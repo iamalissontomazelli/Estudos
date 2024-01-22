82e1fea73300615bc726d0a7193ac28e4507e88b
Alisson  Tomazelli <105504791+iamalissontomazelli@users.noreply.github.com>
2024-01-22 15:28:07 +0000

    first commit
    Upload de files

---
+++ Projetos de JS/Sidebar-Project/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('.menu-btn').click(function () {
+        $('.side-bar').addClass('active');
+        $('.menu-btn').css("visibility", "hidden");
+    });
+    $('.close-btn').click(function () {
+        $('.side-bar').removeClass('active');
+        $('.menu-btn').css("visibility", "visible");
+    });
+})
